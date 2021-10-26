82e0e25b2ee4a17de2c90b71d29864f590e80314
tresorsawasawa <tresorsawasawa@gmail.com>
2021-10-26 11:18:16 +0200

    update scrippt

--- script/script.js
+++ script/script.js
@@ -1,5 +1,20 @@
-const eveDate = "Nov 22, 2021 8:55:59";
+const countDownDate = new Date("Nov 22, 2021 9:55:59");
 
-function countdown() {
-    const startDate = new Date()
+setInterval(setCountDown, 1000);
+
+function setCountDown() {
+    let now = new Date().getTime(),
+        distance = countDownDate - now,
+        milli = 1000;
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (milli * 3600 * 24)),
+        hours = Math.floor((distance % (milli * 3600 * 24)) / (1000 * 60 * 60)),
+        minutes = Math.floor((distance % (milli * 3600)) / (1000 * 60)),
+        seconds = Math.floor((distance % (milli * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
 }
+setCountDown();
